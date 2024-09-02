d29a099a3d25e340cd3ac0e4862d7558fe4b63c1
Traqueur-dev <traqueur.dev@gmail.com>
2024-09-02 21:34:40 +0200

    fix (RuneConfiguration) : disable rune if any of them not allowed mining block

--- src/fr/maxlego08/items/runes/RuneListener.java
+++ src/fr/maxlego08/items/runes/RuneListener.java
@@ -56,8 +56,15 @@ public class RuneListener implements Listener {
         var optional = getRunes(itemStack);
         if (optional.isEmpty()) return;
 
+        var runes = optional.get();
+
+
+        if(runes.stream().anyMatch(rune -> !rune.getConfiguration().contains(event.getBlock().getType()))) {
+            return;
+        }
+
         Map<Location, List<ItemStack>> drops = new HashMap<>();
-        RunePipeline pipeline = new RunePipeline(optional.get());
+        RunePipeline pipeline = new RunePipeline(runes);
         Set<Block> blocks = pipeline.breakBlocks(plugin, event, drops);
         if (blocks.isEmpty()) return;
 
