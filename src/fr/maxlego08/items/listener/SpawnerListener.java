5d9eb9c7ce2f040eaf83d12887df454801c325b2
Maxlego08 <maxlego08@sfr.fr>
2024-09-03 18:25:28 +0200

    :memo: Add tree cutterr and fix spawner

---
+++ src/fr/maxlego08/items/listener/SpawnerListener.java
@@ -0,0 +1,36 @@
+package fr.maxlego08.items.listener;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class SpawnerListener extends ListenerAdapter {
+
+    @Override
+    protected void onBlockPlace(BlockPlaceEvent event, Player player) {
+
+        Block block = event.getBlock();
+        Material material = block.getType();
+        if (material != Material.SPAWNER) return;
+
+        ItemStack itemStack = event.getItemInHand();
+        if (!itemStack.hasItemMeta()) return;
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta instanceof BlockStateMeta blockStateMeta) {
+            if (blockStateMeta instanceof CreatureSpawner creatureSpawner) {
+                EntityType entity = creatureSpawner.getSpawnedType();
+                if (block.getState() instanceof CreatureSpawner spawner) {
+                    spawner.setSpawnedType(entity);
+                    spawner.update();
+                }
+            }
+        }
+    }
+}
